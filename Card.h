3ed13d0031d7eb3a0f6fd14340d11415592a5b9d
Talha Zekeriya Durmuş <drtzack@gmail.com>
2017-12-06 18:06:19 +0100

    card & trick

--- Card.h
+++ Card.h
@@ -2,6 +2,7 @@
 #define FINAL_CARD_H
 
 #include "Color.h"
+#include "Contract.h"
 
 enum CARDVALUE {
     TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
@@ -12,9 +13,24 @@ private:
     CARDVALUE _value;
     COLOR _color;
 public:
-    Card(CARDVALUE value, COLOR color){
+    Card(CARDVALUE value, COLOR color) {
         _value = value;
         _color = color;
+    }
+
+    CARDVALUE getValue() const {
+        return _value;
+    }
+
+    COLOR getColor() const {
+        return _color;
+    }
+
+    bool isBigger(Card *rhs, Contract *c) {
+        if (rhs->getColor() == getColor()) {
+            return getValue() > rhs->getColor();
+        }
+        return getColor() == c->getColor();
     }
 };
 
--- Trick.cpp
+++ Trick.cpp
@@ -3,3 +3,22 @@
 void Trick::addCard(Card *c, int p) {
     _cards.push_back(pair<Card *, int>(c, p));
 }
+
+COLOR Trick::getStartingColor() {
+    if (_cards.size() == 0) {
+        return COLOR::NOTRUMP;
+    } else {
+        return _cards.front().first->getColor();
+    }
+}
+
+int Trick::getWinner(Contract *c) {
+    std::list<pair<Card *, int>>::iterator it = _cards.begin();
+    pair<Card *, int> biggest = *it;
+    for (++it; it != _cards.end(); ++it) {
+        if (it->first->isBigger(biggest.first, c)) {
+            biggest = *it;
+        }
+    }
+    return biggest.second;
+}
