c22f6ecc1f85e2139ef21b632a611e493f1eac12
Talha Zekeriya Durmuş <drtzack@gmail.com>
2017-12-06 17:27:45 +0100

    contract class implemented

--- Contract.cpp
+++ Contract.cpp
@@ -1,1 +1,41 @@
 #include "Contract.h"
+
+bool Contract::operator>(Contract *lhs, Contract *rhs) {
+    if (lhs->getTricksAmount() > rhs->getTricksAmount()) {
+        return true;
+    } else if (lhs->getTricksAmount() > rhs->getTricksAmount()) {
+        return lhs->getColor() > lhs->getColor();
+    }
+    return false;
+}
+
+
+int Contract::getTricksAmount() {
+    return _tricksAmount;
+}
+
+bool Contract::getColor() {
+    return _color;
+}
+
+bool Contract::operator<(Contract *lhs, Contract *rhs) {
+    return !operator>(lhs, rhs);
+}
+
+bool Contract::operator==(Contract *lhs, Contract *rhs) {
+    return lhs->getColor() == rhs->getColor() && lhs->getTricksAmount() == rhs->getTricksAmount();
+}
+
+Contract::Contract(int amountOfTricks, COLOR color) {
+    _tricksAmount = amountOfTricks;
+    _color = color;
+}
+
+Contract::Contract() {
+    _tricksAmount = 0;
+    _color = COLOR::NOTRUMP;
+}
+
+Contract *Contract::Pass() const {
+    return new Contract(); // Todo: Should be singleton
+}
--- Contract.h
+++ Contract.h
@@ -1,23 +1,33 @@
 #ifndef CONTRACT_H_INCLUDED
 #define CONTRACT_H_INCLUDED
 
+#include "Color.h"
+
 class Contract {
-    private:
-        int _tricksAmount;
-        Color _color;
-        //Maybe an option, like double, etc ...
-        static Contract* _pass;
-        Contract();
+private:
+    int _tricksAmount;
+    COLOR _color;
+    //Maybe an option, like double, etc ...
+    static Contract *_pass;
 
-    public:
-        static Contract* const Pass();
-        Contract(int amountOfTricks, Color color);
-        bool operator>(Contract* c1, Contract* c2);
-        bool operator<(Contract* c1, Contract* c2);
-        bool operator==(Contract* c1, Contract* c2);
-        //void setOption(ContractOption co);
-        //ContractOption getContractOption();
+    Contract();
 
+public:
+    static Contract *Pass() const;
+
+    Contract(int amountOfTricks, COLOR color);
+
+    bool operator>(Contract *lhs, Contract *rhs);
+
+    bool operator<(Contract *lhs, Contract *rhs);
+
+    bool operator==(Contract *lhs, Contract *rhs);
+
+    int getTricksAmount();
+    //void setOption(ContractOption co);
+    //ContractOption getContractOption();
+
+    bool getColor();
 };
 
 #endif // CONTRACT_H_INCLUDED
